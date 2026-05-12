6c3b4f856a268078fbb7704ddf036834074c6bcd
Fabian Baena <fabian.baena@samanagroup.com>
2026-05-12 11:06:14 -0400

    added static files in the package

--- sammmanager/__init__.py
+++ sammmanager/__init__.py
@@ -168,6 +168,13 @@ def gettoken(**kwargs):
 
 def private(**kwargs):
 	log.debug("Private request: kwargs='%s'", str(kwargs))
+	action = kwargs.get("action")
+	if action == "rename":
+		old_name=kwargs.get("old_name")
+		new_name=kwargs.get("new_name")
+		if old_name == "" or new_name == "":
+			raise Exception("Invalid parameters")
+		return rename_file(old_name, new_name)
 	filepath = Path(__file__).parent / "docs/samm-file-manager.html"
 	with filepath.open("r") as f:
 		temp = Template(f.read())
@@ -185,5 +192,15 @@ def private(**kwargs):
 	return ("200 OK",
 		[
 			("Content-Type", "text/html; charset=utf-8"),
+			("Content-Length", str(len(body))),
+		], body)
+
+def rename_file(old_name, new_name):
+	log.debug("Renaming file %s to %s", old_name, new_name)
+	out = { error: "", details: "File renamed"}
+	body = json.dumps(out).encode("utf-8")
+	return ("200 OK",
+		[
+			("Content-Type", "application/json; charset=utf-8"),
 			("Content-Length", str(len(body))),
 		], body)
