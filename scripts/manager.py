a208ccb51b2000d5891db83b55e8eb8d0b9d4eef
Fabian Baena <fabian.baena@samanagroup.com>
2026-05-13 11:15:32 -0400

    added static files in the package

--- scripts/manager.py
+++ scripts/manager.py
@@ -25,6 +25,7 @@ def application(env, start_response):
 	log.info("Requests received. data=%s", env)
 	try:
 		func_name = path_info.relative_to(basepath).parent
+		log.debug("request=%s", path_info.relative_to(basepath))
 		if func_name == ".":
 			func_name = path_info.relative_to(basepath).name
 		else:
