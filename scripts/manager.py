6d95c45597139a85d6bb07433bd4ad99a7f198bd
Fabian Baena <fabian.baena@samanagroup.com>
2026-05-14 09:25:50 -0400

    added static files in the package

--- scripts/manager.py
+++ scripts/manager.py
@@ -28,7 +28,7 @@ def process_session(env, basepath, token):
 
 	if isinstance(token, list):
 		token = token[0]
-	log.debug("token='%s'", token)
+	log.debug("query_token='%s' session_token='%s", token, sammcookie.value if sammcookie is not None else "none")
 
 	if sammcookie is None and token is None:
 		raise NotAuthorized
